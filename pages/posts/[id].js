804a31c35ff605e79f4df452a59b5ebb3becfff1
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-06-05 22:56:54 +0900

    update: markdownをレンダリング

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
@@ -26,7 +28,7 @@ export async function getStaticPaths() {
 // 静的データの生成
 export async function getStaticProps({ params }) {
   // params.idを使用して、ブログの投稿に必要なデータを取得する
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
