8c7e257e6bfd8578ee32469c7092172b8c71c77a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-06-05 22:46:06 +0900

    update: 動的ルーティング対応

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+// 動的ルーティング対応(ルーティングを受け付けるページの[id]リストを生成)
+export async function getStaticPaths() {
+  // idとしてとりうる値のリストを返す
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+// 静的データの生成
+export async function getStaticProps({ params }) {
+  // params.idを使用して、ブログの投稿に必要なデータを取得する
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
