7c9b902e9d35ae29bf8108411a618d2a3e366b17
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-06-11 00:54:41 +0900

    chore

--- components/date.tsx
+++ components/date.tsx
@@ -1,7 +1,11 @@
 import { FunctionComponent } from 'react'
 import { parseISO, format } from 'date-fns'
 
-export const Date: FunctionComponent<{ dateStr: string }> = ({ dateStr }) => {
+interface Props {
+  dateStr: string
+}
+
+export const Date: FunctionComponent<Props> = ({ dateStr }) => {
   const date = parseISO(dateStr)
   return <time dateTime={dateStr}>{format(date, 'yyyy-M-d LLLL')}</time>
 }
