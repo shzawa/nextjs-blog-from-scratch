ec5557498cd50794c92ab0f9d11e915d8a644d33
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-06-13 22:35:35 +0900

    update: タグ絞り込み時にタグ名強調表示

--- pages/tags/[tag].tsx
+++ pages/tags/[tag].tsx
@@ -60,7 +60,11 @@ const TagListPage: FunctionComponent<Props> = ({ posts, tag }) => {
     return (
       <TagPageLayout title={setTitle(tag)}>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Posts - Filter by {tag}</h2>
+          <h2 className={utilStyles.headingLg}>
+            Posts - Filter by {`“`}
+            {tag}
+            {`”`}
+          </h2>
           Now Loading...
         </section>
       </TagPageLayout>
@@ -70,7 +74,11 @@ const TagListPage: FunctionComponent<Props> = ({ posts, tag }) => {
   return (
     <TagPageLayout title={setTitle(tag)}>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Posts - Filter by {tag}</h2>
+        <h2 className={utilStyles.headingLg}>
+          Posts - Filter by {`“`}
+          {tag}
+          {`”`}
+        </h2>
         <ul className={utilStyles.list}>
           {posts.map(({ id, date, title, tags }) => (
             <li className={utilStyles.listItem} key={id}>
