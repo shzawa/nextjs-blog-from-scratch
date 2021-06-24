c34ad3dd31eae5abbff1e19d37e6747cdb8f8eca
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-06-24 21:29:17 +0900

    add: Header/index.tsx

---
+++ src/components/layouts/Header/index.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
+import { site } from '../../../constants/site'
+
+export const Header: FunctionComponent = () => (
+  <StyledHeader>
+    <StyledH2>
+      <Link href="/">
+        <a color={`inherit`}>{site.name}</a>
+      </Link>
+    </StyledH2>
+  </StyledHeader>
+)
+
+const StyledHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const StyledH2 = styled.h2`
+  font-size: 1.5rem;
+  line-height: 1.4;
+  margin: 1rem 0;
+`
