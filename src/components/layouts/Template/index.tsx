5cf2c9b377f86b15d67ff9c328c95c1f39c0df13
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-06-24 21:36:40 +0900

    update: Templateをindexに移管

--- src/components/layouts/Template/index.tsx
+++ src/components/layouts/Template/index.tsx
@@ -7,6 +7,8 @@ import { TitleWithSiteTitle } from '../../title'
 import { HeaderProfile } from '../../header'
 import { BackToPostsBtn, BackToPreviousBtn } from '../../button'
 import styles from './layout.module.css'
+import { site } from '../../../constants/site'
+import { Header } from '../Header/index'
 
 export const siteTitle = 'Fragment'
 
@@ -82,6 +84,48 @@ export const ErrorPageLayout: FunctionComponent<Props> = ({
       <BackToPostsBtn />
     </div>
   </Container>
+)
+
+interface TemplateProps {
+  title: string
+  description: string
+}
+
+export const Template: FunctionComponent<TemplateProps> = ({
+  children,
+  title,
+  description,
+}) => (
+  <>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, height=device-height, minimum-scale=1.0, initial-scale=1.0"
+      />
+
+      <meta name="og:title" content={site.name} />
+      <meta
+        property="og:image"
+        content={`https://og-image.now.sh/${encodeURI(
+          site.name,
+        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+      />
+      <meta property="og:site_name" content={site.name} />
+      <meta property="og:locale" content="ja_JP" />
+
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@tk_zawa" />
+    </Head>
+    <Container>
+      <Header />
+      <main>{children}</main>
+    </Container>
+  </>
 )
 
 const StyledContainer = styled.div`
